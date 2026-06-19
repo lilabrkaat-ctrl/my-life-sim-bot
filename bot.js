78a4c2cbee3f4e1e9b69fd88069b5a6928eefa4b
lilabrkaat-ctrl <lilabrkaat@gmail.com>
2026-06-19 15:52:22 +0330

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,2 @@
-const { Bot } = require("grammy");
-const TOKEN = process.env.BOT_TOKEN || "TOKEN_DEFAULT";
-const bot = new Bot(TOKEN);
 
-bot.command("start", async (ctx) => {
-    await ctx.reply("✅ سلام! ربات کار می‌کنه!");
-});
-
-bot.start({ onStart: (info) => console.log(`✅ @${info.username}`) });
+require("./src/bot");
